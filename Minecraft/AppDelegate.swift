91f17bf99acf3d8eeffcaf9c1c21c0ba222a1fea
nixzhu <zhuhongxu@gmail.com>
2017-01-06 22:53:08 +0800

    init

---
+++ Minecraft/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Minecraft
+//
+//  Created by nixzhu on 2017/1/6.
+//  Copyright © 2017年 nixWork. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ Redstone/Redstone.h
@@ -0,0 +1,19 @@
+//
+//  Redstone.h
+//  Redstone
+//
+//  Created by nixzhu on 2017/1/6.
+//  Copyright © 2017年 nixWork. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Redstone.
+FOUNDATION_EXPORT double RedstoneVersionNumber;
+
+//! Project version string for Redstone.
+FOUNDATION_EXPORT const unsigned char RedstoneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Redstone/PublicHeader.h>
+
+
